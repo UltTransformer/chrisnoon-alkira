5f2faa34f7ff0a593ae2bcb8e11a785b578f0a4a
Chris Noon <chris.g.noon@gmail.com>
2024-02-20 15:34:54 +0000

    baseline vnet creation

---
+++ Azure/vNET/main.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  features = {}
+}
+
+resource "azurerm_resource_group" "chrisnoon-uk-south" {
+  name     = "chrisnoon-uk-south"
+  location = "UK South"
+}
+
+resource "azurerm_virtual_network" "chrisnoon-vnet" {
+  name                = "chrisnoon-vnet"
+  address_space       = ["10.21.0.0/16"]
+  location            = azurerm_resource_group.chrisnoon-uk-south.location
+  resource_group_name = azurerm_resource_group.chrisnoon-uk-south.name
+}
+
+resource "azurerm_subnet" "example" {
+  count               = 2
+  name                = "subnet-${count.index}"
+  resource_group_name = azurerm_resource_group.chrisnoon-uk-south.name
+  virtual_network_name = azurerm_virtual_network.chrisnoon-uk-south.name
+  address_prefixes    = ["10.21.${count.index + 1}.0/24"]
+}

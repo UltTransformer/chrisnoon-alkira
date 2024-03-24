cdb7f2e9ad55608ae56517835e03203e888960a3
Chris Noon <chris.g.noon@gmail.com>
2024-03-24 09:48:16 +0000

    adding ec2 instance to vpc

--- AWS/VPC/EC2.tf
+++ AWS/VPC/EC2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "chrisnoon-ec2-1" {
   ami           = "ami-0e58172bedd62916b" 
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.chrisnoon-subnet1.vpc_id 
+  subnet_id     = aws_subnet.chrisnoon-subnet1.subnet_id 
   tags = {
     Name = "chrisnoon-ec2-1"  # Set your desired instance name tag
   }

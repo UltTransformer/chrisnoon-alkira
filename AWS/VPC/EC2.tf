907ebf6e4fe5b09bac02e61f60b625da29220d54
Chris Noon <chris.g.noon@gmail.com>
2024-03-25 11:18:20 +0000

    adding vm instance to vnet

--- AWS/VPC/EC2.tf
+++ AWS/VPC/EC2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "chrisnoon-ec2-1" {
-  ami           = "ami-0e58172bedd62916b" 
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.chrisnoon-subnet1.id 
-  tags = {
-    Name = "chrisnoon-ec2-1"  # Set your desired instance name tag
-  }
-}
+# resource "aws_instance" "chrisnoon-ec2-1" {
+#   ami           = "ami-0e58172bedd62916b" 
+#   instance_type = "t2.micro"
+#   subnet_id     = aws_subnet.chrisnoon-subnet1.id 
+#   tags = {
+#     Name = "chrisnoon-ec2-1"  # Set your desired instance name tag
+#   }
+# }

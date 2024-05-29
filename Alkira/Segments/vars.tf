7451ed47b46d38dd9b8270d9b1cf0726a146b9a5
Chris Noon <chris.g.noon@gmail.com>
2024-05-29 15:48:22 +0200

    adding auth for env0

--- Alkira/Segments/vars.tf
+++ Alkira/Segments/vars.tf
@@ -1,9 +1,9 @@
-# Variables
+# # Variables
 
-variable "env0_var_portal" {
-  type    = string
-}
+# variable "env0_var_portal" {
+#   type    = string
+# }
 
-variable "env0_var_api_key" {
-  type    = string
-}
+# variable "env0_var_api_key" {
+#   type    = string
+# }

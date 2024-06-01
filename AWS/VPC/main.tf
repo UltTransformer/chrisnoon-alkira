e28420dd3f17a29b235b4df0278363489259db27
Chris Noon <chris.g.noon@gmail.com>
2024-06-01 20:04:58 +0100

    adding student vpcs

--- AWS/VPC/main.tf
+++ AWS/VPC/main.tf
@@ -2,10 +2,17 @@
 
 provider "aws" {
   region = "eu-west-2"
+  alias = "eu-west-2"
+}
+
+provider "aws" {
+  region = "ap-southeast"
+  alias = "ap-southeast"
 }
 
 # Create Chris Noon's VPC
 resource "aws_vpc" "chrisnoon-vpc" {
+  provider = aws.eu-west-2
   cidr_block = "10.12.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
@@ -38,6 +45,7 @@ resource "aws_subnet" "chrisnoon-subnet2" {
 
 # Create Phil Oultram's VPC
 resource "aws_vpc" "philoultram-vpc" {
+  provider = aws.eu-west-2
   cidr_block = "10.22.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
@@ -70,6 +78,7 @@ resource "aws_subnet" "philoultram-subnet2" {
 
 # Create Ian Plummer's VPC
 resource "aws_vpc" "ianplummer-vpc" {
+  provider = aws.eu-west-2
   cidr_block = "10.32.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
@@ -97,3 +106,34 @@ resource "aws_subnet" "ianplummer-subnet2" {
     Name = "ianplummer-subnet2"
   }
 }
+
+########################################################
+# Cusotmer Lab
+
+# # Student 1
+# resource "aws_vpc" "sydney-vpc" {
+#   provider = aws.ap-southeast
+#   cidr_block = "10.100.1.0/24"
+#   enable_dns_support = true
+#   enable_dns_hostnames = true
+#   tags = {
+#     Name = "sydney-vpc"
+#   }
+# }
+# resource "aws_subnet" "sydney-subnet1" {
+#   vpc_id                  = aws_vpc.sydney-vpc.id
+#   cidr_block              = "10.100.1.0/25"
+#   availability_zone       = "ap-southeast-2"
+#   tags = {
+#     Name = "sydney-subnet1"
+#   }
+# }
+# resource "aws_subnet" "sydney-subnet2" {
+#   vpc_id                  = aws_vpc.sydney-vpc.id
+#   cidr_block              = "10.100.1.128/25"
+#   availability_zone       = "ap-southeast-2"
+#   tags = {
+#     Name = "sydney-subnet2"
+#   }
+# }
+
--- Alkira/CloudConnectors/AWS/main.tf
+++ Alkira/CloudConnectors/AWS/main.tf
@@ -1,15 +1,34 @@
 # AWS Cloud Connector
 
+# Demo code
+# Normal
+# resource "alkira_credential_aws_vpc" "account1" {
+#   name           = "customer-aws-1"
+#   aws_access_key = "your_aws_acccess_key"
+#   aws_secret_key = "your_secret_key"
+#   type           = "ACCESS_KEY"
+# }
+
+# # Using environment variables
+# export AWS_ACCESS_KEY_ID=XXX
+# export AWS_SECRET_ACCESS_KEY=XXX
+
+# resource "alkira_credential_aws_vpc" "account1" {
+#   name           = "customer-aws-1"
+#   type           = "ACCESS_KEY"
+# }
+
 resource "alkira_connector_aws_vpc" "chrisnoon-vpc" {
   name           = "chrisnoon-vpc"
-  vpc_id         = "your_vpc_id"
+  vpc_id         = "vpc-021eec53052212bdc"
+  vpc_cidr       = [ "10.12.0.0/16" ]
 
-  aws_account_id = "your_aws_account_id"
-  aws_region     = "us-east-1"
+  aws_account_id = "138319602003"
+  aws_region     = "eu-west-2"
 
-  credential_id  = alkira_credential_aws_vpc.account1.id
-  cxp            = "US-WEST"
-  group          = alkira_group.group1.name
-  segment_id     = alkira_segment.segment1.id
+  credential_id  = "5207685e-cf39-4253-b9b0-84a4b72a046a"
+  cxp            = "US-WEST-1"
+  group          = "chrisnoon-aws"
+  segment_id     = "9397"
   size           = "SMALL"
 }
